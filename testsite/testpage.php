50d23c29c017e1cddf834abfde9e1cc3eac897a0
pepijnbloem1998 <63612000+pepijnbloem1998@users.noreply.github.com>
2022-06-03 11:55:05 +0200

    Add files via upload

--- testsite/testpage.php
+++ testsite/testpage.php
@@ -8,7 +8,7 @@
     {
       if($_POST['txt_naam']!="" && $_POST['txt_password']!="")
       {
-        $url="http://10.3.27.37:5051/valideer/" .$_POST['txt_naam']. "/" .$_POST['txt_password'];
+        $url="http://10.0.2.5:5051/valideer/" . $_POST['txt_naam'] . "/" . $_POST['txt_password'];
         $html1=file_get_contents($url);
       }
       else
@@ -18,17 +18,18 @@
     }
   if($focus==2)
    {
-     $url="http://10.3.27.32:5052/hoofdstad/" .$_POST['txt_land'];
-     $html2="hoofdstad van " .$_POST['txt_land']. " is " . file_get_contents($url);
+     $url="http://10.0.2.5:5052/hoofdstad/" . $_POST['txt_land'];
+     $html2="hoofdstad van " . $_POST['txt_land'] . " is " . file_get_contents($url);
    }
+
 ?>
- <!DOCTYPE html>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
 </head>
-
 
 <body>
    <h1>Testapplicatie voor DEVOPS, VERSIE-01</h1>
